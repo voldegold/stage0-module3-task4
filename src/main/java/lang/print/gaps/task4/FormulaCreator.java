1b9ecf70ea3e38d3b3c0dd0db9c0a65696d30e07
voldegold <kva1107@gmail.com>
2022-07-29 00:40:33 +0600

    all solved

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=1, b=2;
+        int c = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        char C = (char) c;
+        int h = 104;
+        char H = (char) h;
+        int a = 97;
+        char A = (char) a;
+        int r = 114;
+        char R = (char) r;
+
+
     }
 }
